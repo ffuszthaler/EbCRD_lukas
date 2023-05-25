48214a4971e5f616b92d813b911609231c3b1710
ffuszthaler <flo.fuszthaler@gmail.com>
2023-05-25 08:28:53 +0200

    added indoor room scene

---
+++ Assets/Scripts/LevelChange.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChange : MonoBehaviour
+{
+    [SerializeField] private string level;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadSceneAsync(level);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
